d8a00c710d12db8e589a8f229c8f5765da5ad829
Andrew Wong <wongandrew97@gmail.com>
2016-07-03 02:16:49 -0400

    Changed querying calls such that we don't need intermediate json files, we do it through objects one time

--- app/prereqs.py
+++ app/prereqs.py
@@ -1,14 +1,116 @@
 import requests
-import timeit
 import json
+import os
+import time
+from datetime import date
 
 from bs4 import BeautifulSoup
-from query import getNextTerm
 
-def openQuery(path):
-	f = open(path).read()
-	data = json.loads(f)
-	return data
+API_KEY = "Y7YXSplpQcXOWoyB4Z61tki74wNqHu8S"
+
+# Getting current data, adding it to database
+# take some code from query.py
+# 1. Query if not cached
+# 2. create Course objects
+# 3. find prereqs, then create a fixture. 
+# 4. The current fixture is not very efficient and frankly doesn't even work at all
+
+# Basically instead of creating intermediate files, 
+# create fixture by reading the response all in one go
+
+class Course():
+	def __init__(self, data):
+		self.Number = data["Number"][:10]
+		self.ID = data["ID"][:6]
+		self.Title = data["Title"]
+		self.Location = data["Location"]
+		try:
+			self.Credits = float(data["Credits"])
+		except:
+			self.Credits = 0.0
+		self.Level = data["Level"]
+		try:
+			self.Instructors = data["Instructors"].split(",")
+		except:
+			self.Instructors = [data["Instructors"]]
+		self.Prerequisites = []
+		self.Semester = data["Semester"]
+		self.pk = data["pk"]
+
+def getNextTerm():
+	"""
+	Usage: getNextTerm() - uses current date to find the next term.
+	Dates are based on course selection periods.
+	Currently does not support Intersession or Summer.
+
+	:return a string of current term (e.g. Fall 2016)
+	"""
+	currentMonth = int(date.today().strftime("%m"))
+	currentYear = date.today().strftime("%Y")[0:4]
+	nextYear = str(int(currentYear)+1)
+
+	if currentMonth >= 3 and currentMonth < 11:
+		# [3,10]
+		return "Fall " + currentYear
+	elif (currentMonth >= 11 and currentMonth < 13) or currentMonth < 3:
+		# [11,12] and [1,2]
+		return "Spring " + nextYear
+
+def getCurrentCatalog():
+	""" Query the ISIS API for current term courses """
+	catalog = []
+	schools = ["Whiting School of Engineering", "Krieger School of Arts and Sciences"]
+
+	for school in schools:
+		r = requests.get("https://isis.jhu.edu/api/classes/" + school + \
+			"/" + getNextTerm() + "?key=" + API_KEY)
+		response = json.loads(r.content)
+		for i in response:
+			if i not in catalog:
+				catalog.append(i)
+	with open("cache/" + getNextTerm().replace(" ", "") + "_courses.json",'wb') as f:
+		f.write(json.dumps(catalog))
+	f.close()
+
+def createCatalog():
+	""" 
+	Parse current response, put it in Course objects in a dictionary of course numbers 
+	:return dict of course objects
+	"""
+	catalog = {}
+	temp = []
+	
+	# Initialize for error catching block to work
+	response = []	
+	try:
+		response = json.loads(open("cache/" + getNextTerm().replace(" ", "") + "_courses.json").read())
+	except Exception, e:
+		getCurrentCatalog()
+		createCatalog()
+
+	# Make sure the response is current
+	if response[0]["Term"] != getNextTerm():
+		getCurrentCatalog()
+		createCatalog()
+
+	index = 0
+	for i in response:
+		if i["OfferingName"] not in temp and i["Level"]=="Lower Level Undergraduate" or i["Level"]=="Upper Level Undergraduate":
+			catalog[i["OfferingName"]] = Course({
+				"Number": i["OfferingName"],
+				"ID": i["SSS_SectionsID"],
+				"Title": i["Title"],
+				"Location": i["Location"],
+				"Credits": i["Credits"],
+				"Level": i["Level"],
+				"Instructors": i["Instructors"],
+				"Semester": i["Term"],
+				"pk": index
+			})
+			temp.append(i["OfferingName"]) # list of courses that are already added
+			index += 1
+	return catalog
+
 
 def getPrereqs(idNum):
 	url = "https://isis.jhu.edu/classes/ClassDetails.aspx?id=" + idNum;
@@ -21,102 +123,106 @@ def getPrereqs(idNum):
 		return prereqs
 	return []
 
-def getDetails(idNum,attribute):
-	f = open("cache/response.json").read()
-	data = json.loads(f)
-	for i in data:
-		if i["SSS_SectionsID"] == idNum:
-			return i[attribute]
+def searchForCourse(courseNum):
+	""" Search for latest occurence of course and gets its data """
+	return 1
+	return "Fixes need to be done"
+	try:
+		courseNum = courseNum.replace(".","")
+		courseNum = requests.utils.quote(courseNum)
+		url = 'https://isis.jhu.edu/api/classes/' + courseNum + '/' + '?key='
 
-def write(data):
+		r = requests.get(url + API_KEY)
+		data = json.loads(r.content)
+		latest = data[-1]
+		return latest
+	except:
+		return 1
+
+def writeFixture():
+	""" 
+	Writes initial course data for Django 
+
+	To do: fix request speed by saving all course data from Spring 2009 into a repsonse file different from 
+	Fall2016_courses.json
+
+	Then, look for this course number in that data (much, much faster than sending each request to ISIS server)
+	There's probably a better way to do this search, but we'll deal with that later.
+
+
+	"""
+	data = []
+	catalog = createCatalog()
+	largest = 0
+
+	for key in catalog:
+		if catalog[key].pk > largest:
+			largest = catalog[key].pk
+
+	for key in catalog.keys():
+		catalog[key].Prerequisites = getPrereqs(catalog[key].ID) 
+		pks = []
+		for pre in catalog[key].Prerequisites: 
+			if pre in catalog:
+				pks.append(catalog[pre].pk)
+			else:
+				new = searchForCourse(pre) # This request is taking too long. See solution at method docs top
+				if new != 1:
+					largest += 1
+					catalog[pre] = Course({
+						"Number": new["OfferingName"],
+						"ID": new["SSS_SectionsID"],
+						"Title": new["Title"],
+						"Location": new["Location"],
+						"Credits": new["Credits"],
+						"Level": new["Level"],
+						"Instructors": new["Instructors"],
+						"Semester": new["Term"],
+						"pk": largest
+					})
+					pks.append(largest)
+				else:
+					largest += 1
+					catalog[pre] = Course({
+						"Number": pre,
+						"ID": "None",
+						"Title": "None",
+						"Location": "None",
+						"Credits": "None",
+						"Level": "None",
+						"Instructors": "None",
+						"Semester": "None",
+						"Prerequisites": [],
+						"pk": largest
+					})
+					pks.append(largest)
+		catalog[key].Prerequisites = pks
+		print key, catalog[key].Prerequisites
+
+
+	for key in catalog:
+		data.append({
+			"model": "app.Course",
+			"pk": catalog[key].pk,
+			"fields": {
+				"number": catalog[key].Number,
+				"ID": catalog[key].ID,
+				"title": catalog[key].Title,
+				"instructors": catalog[key].Instructors,
+				"location": catalog[key].Location,
+				"credits": catalog[key].Credits,
+				"level": catalog[key].Level,
+				"semester":  catalog[key].Semester,
+			}
+		})
+
+
 	with open("cache/fixture.json", "wb") as f:
 		f.write(json.dumps(data))
 	f.close()	
 
-def makeFixture():
-	rawData = openQuery('cache/Fall2016.json')
-	data = []
-	allNumbers = {} # EN.600.120: 012, Number: pk
-
-	index = 0
-	largest = 0
-	for i in rawData:
-		number = rawData[index]["Number"]
-		ID = rawData[index]["ID"]
-		prereqs = getPrereqs(ID)
-		prereqs = [i[:10] for i in prereqs]
-
-		# Cleaning up some data
-		for i in range(len(prereqs)):
-			if len(prereqs[i]) != 10:
-				try:
-					dept = int(prereqs[i][0:3])
-				except:
-					dept = "None"
-				if dept < 500: 
-					prereqs[i] = "AS." + prereqs[i] 
-				elif dept >= 500:
-					prereqs[i] = "EN." + prereqs[i]
-				elif dept == "None":
-					prereqs.remove(prereqs[i])
-
-		title = rawData[index]["Title"]
-		instructors = getDetails(ID,"Instructors")
-		if isinstance(instructors,list):
-			instructors = instructors.split(",")
-		else:
-			instructors = [instructors]
-		location = rawData[index]["Location"]
-		try:
-			credits = float(rawData[index]["Credits"])
-		except:
-			credits = 0
-		level = rawData[index]["Level"]
-
-		if number not in allNumbers:
-			data.append({
-				"model": "app.Course",
-				"pk": index,
-				"fields": {
-					"number": number,
-					"ID": ID,
-					"prerequisites": prereqs, # replace with pk
-					"title": title,
-					"instructors": instructors,
-					"location": location,
-					"credits": credits,
-					"level": level,
-					"semester": getNextTerm()
-				}
-			})
-			allNumbers[number] = index
-			print number, prereqs
-		index+=1
-		largest = index
-
-	for i in range(0,len(data)-1):
-		prereq_pks = []
-
-		for pre in data[i]["fields"]["prerequisites"]:
-			if pre in allNumbers:
-				prereq_pks.append(allNumbers[pre])
-			else: # append empty prereq
-				largest+=1
-				data.append({
-				"model": "app.Course",
-				"pk": largest,
-				"fields": {
-					"number": pre, # next step: search jhu database for the data that corresponds to this course number
-					"semester": getNextTerm()
-					}
-				})
-				prereq_pks.append(largest)
-				allNumbers[pre] = largest
-		data[i]["fields"]["prerequisites"] = prereq_pks
-		print "Primary Keys:", data[i]["fields"]["prerequisites"]
-	write(data)
-
 if __name__ == '__main__':
-	start = timeit.default_timer()
-	makeFixture()
-	print "Runtime: " + str(timeit.default_timer()-start)[:4] + ' seconds'
+	start = time.time()
+	writeFixture()
+	print "Runtime: %f seconds" %(time.time()-start)
+	
--- app/query.py
+++ app/query.py
@@ -1,58 +1,27 @@
-import requests
-import json
-import os
-from datetime import date
+# DO NOT CONTINUE EDITING, BAD PRACTICE
+# DEPRECATE THIS METHOD
+# 1. Query if not cached
+# 2. create Course objects
+# 3. find prereqs, then create a fixture. 
+# 4. The current fixture is not very efficient and frankly doesn't even work at all
 
-
-def getNextTerm():
-	"""
-	Usage: getNextTerm() - uses current date to find the next term.
-	Dates are based on course selection periods.
-	Currently does not support Intersession or Summer.
-
-	Returns a string of current term
-	"""
-	currentMonth = int(date.today().strftime("%m"))
-	currentYear = date.today().strftime("%Y")[0:4]
-	nextYear = str(int(currentYear)+1)
-
-	if currentMonth >= 3 and currentMonth < 11:
-		# [3,10]
-		return "Fall " + currentYear
-	elif (currentMonth >= 11 and currentMonth < 13) or currentMonth < 3:
-		# [11,12] and [1,2]
-		return "Spring " + nextYear
-
-def getCurrentCatalog():
-	"""
-	Caches current term response.json course catalog
-	"""
-	catalog = []
-	schools = ["Whiting School of Engineering", "Krieger School of Arts and Sciences"]
-	api_key = "Y7YXSplpQcXOWoyB4Z61tki74wNqHu8S"
-
-	for school in schools:
-		r = requests.get("https://isis.jhu.edu/api/classes/" + school + \
-			"/" + getNextTerm() + "?key=" + api_key)
-		response = json.loads(r.content)
-		for i in response:
-			if i not in catalog:
-				catalog.append(i)
-	with open("response.json",'wb') as f:
-		f.write(json.dumps(catalog))
-	f.close()
 
 def createCatalog():
 	"""
 	Returns list of all current courses and their attributes in WSE and KSAS undergrad
+
 	"""
 	catalog = []
 	temp = []
 	response = []
 
+	if response[0]["Term_IDR"] != getNextTerm():
+		getCurrentCatalog()
+		createCatalog
+
 	try:
 		response = json.loads(open("response.json").read())
-	except Exception or response[0]["Term_IDR"] != getNextTerm():
+	except Exception, e:
 		getCurrentCatalog()
 		createCatalog()
 
@@ -71,10 +40,13 @@ def createCatalog():
 
 
 def queryToFile():
+	"""
+	You need to remove queryToFile and createCatalog and instead integrate that with prereqs.py
+	"""
 	term = getNextTerm()
 	catalog = createCatalog()
 	if catalog!=-1:
-		with open(term.replace(" ","")+".json", "wb") as f:
+		with open("cache/" + term.replace(" ","") + ".json", "wb") as f:
 			try:
 				f.write(json.dumps(catalog))
 			except Exception:
